f8c1d6edc7988d29c979440ed5404da1923f4d56
heisenberg-meth <nmdevi63@gmail.com>
2026-06-05 11:38:03 +0530

    refactor: remove redundant logger type definition and fix import statement in tracing config

--- src/shared/utils/logger.d.ts
+++
@@ -1,8 +0,0 @@
-declare const logger: {
-  info: (message: string) => void;
-  error: (message: string) => void;
-  warn: (message: string) => void;
-  debug: (message: string) => void;
-};
-
-export default logger;
